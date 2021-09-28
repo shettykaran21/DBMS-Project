344490bfea8a71df4e280bd22dc100aee24036f2
Nihaal Shetty <67154176+nihalshetty29@users.noreply.github.com>
2021-09-28 19:08:04 +0530

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,19 @@
+<?php
+// include header.php file
+include('header.php');
+?>
+
+<?php
+/*  include cart items if it is not empty */
+include('Template/_cart-template.php');
+/*  include cart items if it is not empty */
+
+/*  include top sale section */
+include('Template/_new-phones.php');
+/*  include top sale section */
+?>
+
+<?php
+// include footer.php file
+include('footer.php');
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include('header.php');
+?>
+
+<?php
+
+/*  include banner area  */
+include('Template/_banner-area.php');
+/*  include banner area  */
+
+/*  include top sale section */
+include('Template/_top-sale.php');
+/*  include top sale section */
+
+/*  include banner ads  */
+include('Template/_banner-ads.php');
+/*  include banner ads  */
+
+/*  include new phones section  */
+include('Template/_new-phones.php');
+/*  include new phones section  */
+
+?>
+
+<?php
+include('footer.php');
+?>
